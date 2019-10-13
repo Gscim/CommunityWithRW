2101e1d32444b70c49af1eba955c0e55f1a5aab0
Gscim <gscim.d@pku.edu.cn>
2019-10-13 17:53:36 +0800

    data reader modified

--- DataReader.py
+++ DataReader.py
@@ -1,5 +1,6 @@
 import os
-
+'''
+'''
 class DataReader:
     def __init__(self, file_path: str):
         self.file_path = file_path
@@ -12,11 +13,58 @@ class DataReader:
         rwList = []
         for line in rwfile:
             linetrip = line.strip().split(" ")
-            linetrip
             rwList.append(list(map(eval, linetrip)))
         
+        rwfile.close()
         return rwList
 
+    def readGraphFile(self):
+        if not os.path.exists(self.file_path):
+            print("no random graph file exists, please check file path...")
+        if self.file_path.endswith('.formu'):
+            return self.readFormulatedGraph()
+        else:
+            portion = os.path.splitext(self.file_path)
+            formu_file = portion[0] + '.formu'
+            if os.path.exists(formu_file):
+                return self.readFormulatedGraph(form_file=formu_file)
+            else:
+                return self.formulateGraphFile()
+
+    def readFormulatedGraph(self, form_file = self.file_path) -> list:
+        if not os.path.exists(form_file):
+            print("no formulated graph file exists, please check file path...")
+        
+        graphList = []
+        gfile = open(form_file, 'r')
+        for line in gfile:
+            linetrip = line.strip().split(" ")
+            graphList.append(list(map(eval, linetrip)))
+
+        gfile.close()
+        return graphList
+    
+    def formulateGraphFile(self) -> list:
+        if not os.path.exists(self.file_path):
+            print("no graph file exists, please check file path...")
+        
+        portion = os.path.splitext(self.file_path)
+        formu_path = portion[0] + '.formu'
+        graph_file = open(self.file_path, 'r')
+        formu_file = open(formu_path, 'w')
+        reList = []
+
+
+
+
+
+
+        graph_file.close()
+        formu_file.close()
+        return reList
+
+
+   
 if __name__ == "__main__":
     dr = DataReader("trialout.bin")
     print(dr.readRWFile())
