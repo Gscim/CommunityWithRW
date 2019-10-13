5750485ba6a0eb08e5b82682191a6fec78c77efc
Gscim <gscim.d@pku.edu.cn>
2019-10-13 15:51:10 +0800

    random walks file trial

--- RandomWalks.py
+++ RandomWalks.py
@@ -1,1 +1,24 @@
-import ramdom.choice
+'''
+Generate random walks from a given graph file 
+walk input file of type {{node1:adjnode1, adjnode2,...},...}
+'''
+
+from ramdom import choice
+import math
+
+class RandomWalks:
+    def __init__(self, walk_length, walk_input, walk_output):
+        self.walk_length = walk_length
+        self.walk_input = walk_input
+        self.walk_output = walk_output
+
+    def walk_forward(self, start_point:int) -> list:
+        current_node = start_point
+        walkOneRun = []
+        for step in range(self.walk_length):
+            next_step = choice(walk_input[current_node])
+            walkOneRun.add(next_step)
+        return walkOneRun
+    
+    
+
