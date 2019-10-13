3a808a9e04f0ae43e89e6df9c41f7ff194ee5cd6
Gscim <gscim.d@pku.edu.cn>
2019-10-13 16:54:59 +0800

    rw file generator finished

--- RandomWalks.py
+++ RandomWalks.py
@@ -3,8 +3,9 @@ Generate random walks from a given graph file
 walk input file of type {{node1:adjnode1, adjnode2,...},...}
 '''
 
-from ramdom import choice
+from random import choice
 import math
+import os
 
 class RandomWalks:
     def __init__(self, walk_input, walk_output, walk_length:int, num_walks:int):
@@ -15,19 +16,26 @@ class RandomWalks:
 
     def walk_forward(self, start_point:int) -> list:
         current_node = start_point
-        walkOneRun = []
+        walkOneRun = [str(start_point)]
         for step in range(self.walk_length):
-            next_step = choice(walk_input[current_node])
-            walkOneRun.append(next_step)
+            # print(current_node)
+            # print(self.walk_input[current_node])
+            next_step = choice(self.walk_input[current_node])
+            walkOneRun.append(str(next_step))
+            current_node = next_step
         return walkOneRun
     
     def generate_rw(self, start_list : list):
-        open(walk_output, mode='wb') as wkout:
-            for start_point in start_list:
-                for nwk in range(self.num_walks):
-                    wkout.write(walk_forward(start_point))
+        wkout = open(self.walk_output, mode='w')
+        for start_point in start_list:
+            for nwk in range(self.num_walks):
+                wkout.write(' '.join(self.walk_forward(start_point)) + '\n')
         
         wkout.close()
-    
+
+if __name__ == "__main__":
+    a = [[1, 2 , 3, 4, 5], [0, 2, 3, 4, 5], [0, 1, 3, 4, 5], [0, 1, 2, 4, 5], [0, 1, 2, 3, 5], [0, 1, 2, 3, 4]]
+    rw = RandomWalks(a, "trialout.bin", 3, 1)
+    rw.generate_rw([0, 1, 2, 3, 4, 5])
 
 
